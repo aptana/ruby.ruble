08bd539cc9c1f5189c07a85234055658d58d8355
sgtcoolguy <cwilliams@aptana.com>
2010-01-04 13:50:36 -0500

    Add first ported command: Validate Syntax

--- bundle.rb
+++ bundle.rb
@@ -2,14 +2,13 @@ require 'radrails'
 
 # its ruby, so this just addscommands/snippets in bundle (or replaces those with same name)
 # many ruby files could add to a single bundle
-RadRails.current_bundle do |bundle|
-  bundle.name = "Ruby"
+bundle 'Ruby' do |bundle|
   bundle.author = "James Edward Gray II et al"
-  bundle.author_email_rot13 = wnzrf@tenlcebqhpgvbaf.arg
+  bundle.author_email_rot13 = "wnzrf@tenlcebqhpgvbaf.arg"
   bundle.copyright = <<END
-Â© Copyright 2009 Aptana Inc. Distributed under GPLv3 and Aptana Source license.
+© Copyright 2009 Aptana Inc. Distributed under GPLv3 and Aptana Source license.
 
-Portions Â© Copyright 2006 James Edward Gray II, distributed under the terms of the MIT License.
+Portions © Copyright 2006 James Edward Gray II, distributed under the terms of the MIT License.
 END
 
   bundle.description = <<END
@@ -25,11 +24,13 @@ END
     # this menu should be shown when any of the following scopes is active:
     menu.scope = [ "source.ruby", "project.rails" ]
     
-    menu.menu "FIXME" do |submenu|
-      # command/snippet names must be unique within bundle and are case insensitive
-      submenu.command "FIXME"
-      submenu.separator
-      submenu.command "FIXME"
-    end
+    #menu.command "Run"
+    #menu.command "Run Focused Unit Test"
+    #menu.separator
+    #menu.command "Documentation for Word"
+    #menu.command "RDoc"
+    #menu.separator
+    #menu.command "Open require"
+    menu.command "Validate Syntax"
   end
 end
