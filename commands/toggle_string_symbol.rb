6462a4ebfb317d6bce6eec5f31eba39e96ba74ab
sgtcoolguy <cwilliams@aptana.com>
2010-01-05 15:00:36 -0500

    change scope (since scope from Textmate is more advanced than we provide)

--- commands/toggle_string_symbol.rb
+++ commands/toggle_string_symbol.rb
@@ -4,7 +4,7 @@ command "Toggle String / Symbol" do |cmd|
   cmd.key_binding = [ :M1, ":" ] # FIXME Keybinding is incorrect
   cmd.output = :replace_selection
   cmd.input = :selection, :scope
-  cmd.scope = "source.ruby"
+  cmd.scope = "source.ruby string.quoted, source.ruby constant.other.symbol.ruby"
   cmd.invoke do |context|
     case str = context.in.read
       # Handle standard quotes
