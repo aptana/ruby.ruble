df5391744df420892ebb0fb9cdb6e5f53e3c0390
sgtcoolguy <cwilliams@aptana.com>
2010-01-07 13:17:47 -0500

    convert each_slice macro into a command

---
+++ commands/each_slice.rb
@@ -0,0 +1,26 @@
+require 'radrails'
+require 'radrails/editor'
+
+command 'each_slice(..) { |group| .. }' do |cmd|
+  cmd.trigger = 'eas'
+  cmd.scope = 'source.ruby'
+  cmd.output = :insert_as_snippet
+  cmd.input = :document
+  cmd.invoke do |context|
+    # Insert enumerator requires
+    CURSOR = [0xFFFC].pack("U").freeze
+    line, col = ENV["TM_LINE_NUMBER"].to_i - 1, ENV["TM_LINE_INDEX"].to_i
+    code = context.in.read.to_a
+    unless ENV.has_key?('TM_SELECTED_TEXT')
+      if code[line].nil?  # if cursor was on the last line and it was blank
+        code << CURSOR
+      else
+        code[line][col...col] = CURSOR
+      end
+    end
+    code = code.join
+    output = RubyRequires.add_requires(code, "enumerator")
+    # FIXME Need to replace the tab trigger prefix!
+    output.split(CURSOR).join('each_slice(${1:2}) { |${2:group}| $0 }')
+  end
+end
