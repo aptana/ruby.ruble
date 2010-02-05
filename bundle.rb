297c6fd06b5c4b2759219d89f702b1720d0befdf
sgtcoolguy <cwilliams@aptana.com>
2010-02-05 12:17:51 -0500

    try to avoid inifnite recursion on aliasing to_env

--- bundle.rb
+++ bundle.rb
@@ -64,17 +64,19 @@ end
 # Extend Ruble::Editor to add special ENV vars
 module Ruble
   class Editor
-    alias :to_env_pre_ruby_bundle :to_env
-    def to_env
-      env_hash = to_env_pre_ruby_bundle
-      scopes = current_scope.split(' ')
-      if !scopes.select {|scope| scope.start_with? "source.ruby" }.empty?
-        env_hash['TM_COMMENT_START'] = "# "
-        env_hash['TM_COMMENT_END'] = ""
-        env_hash['TM_COMMENT_START_2'] = "=begin"
-        env_hash['TM_COMMENT_END_2'] = "=end"
+    unless method_defined?(:to_env_pre_rails_bundle)
+      alias :to_env_pre_ruby_bundle :to_env
+      def to_env
+        env_hash = to_env_pre_ruby_bundle
+        scopes = current_scope.split(' ')
+        if !scopes.select {|scope| scope.start_with? "source.ruby" }.empty?
+          env_hash['TM_COMMENT_START'] = "# "
+          env_hash['TM_COMMENT_END'] = ""
+          env_hash['TM_COMMENT_START_2'] = "=begin"
+          env_hash['TM_COMMENT_END_2'] = "=end"
+        end
+        env_hash
       end
-      env_hash
     end
   end
 end
